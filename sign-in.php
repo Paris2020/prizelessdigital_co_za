b827b4abcf013403ff78fe66450ecfde17aa2cca
Paris2020 <nelly.moseki@amazeelabs.com>
2019-05-22 12:27:42 +0200

    PD: Modified sign-in.php file with correct urls

--- sign-in.php
+++ sign-in.php
@@ -6,11 +6,27 @@
   <meta name="description" content="Sign up and login form.">
   <meta name="author" content="Nelly Moseki, Junior Frontend Developer">
   <title>Prizeless Digital</title>
-  <link rel="stylesheet" href="libraries/bootstrap-4.3.1-dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="bootstrap-4.3.1-dist/css/bootstrap.min.css">
   <link rel="stylesheet" href="themes/pricelessdigital/css/style.css">
 </head>
 <body>
 
+<!-- Signin Form -->
 
+<div class="container">
+  <div class="row justify-content-center">
+    <img src="themes/prizelessdigital/images/logo.jpg"/>
+    <form action="include/signup.inc.php" method="POST">
+
+      <input type="email" name="email" class="form-control" placeholder="Enter email address"><br>
+      <input type="password" name="password" class="form-control" placeholder="Enter password"><br>
+      <input type="submit" name="submit" value="Sign In" class="btn btn-primary"><br>
+    </form>
+  </div>
+</div>
+
+
+<!-- Javascript files -->
+<script src="bootstrap-4.3.1-dist/js/bootstrap.min.js"></script>
 </body>
 </html>
