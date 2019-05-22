f71b0df564294cd64990b1f707688674e618e064
Paris2020 <nelly.moseki@amazeelabs.com>
2019-05-22 12:26:59 +0200

    PD: Modified index.php file with correct urls

--- index.php
+++ index.php
@@ -6,11 +6,20 @@
   <meta name="description" content="Sign up and login form.">
   <meta name="author" content="Nelly Moseki, Junior Frontend Developer">
   <title>Prizeless Digital</title>
-  <link rel="stylesheet" href="libraries/bootstrap-4.3.1-dist/css/bootstrap.min.css">
-  <link rel="stylesheet" href="themes/pricelessdigital/css/style.css">
+  <link rel="stylesheet" href="bootstrap-4.3.1-dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="themes/prizelessdigital/css/style.css">
 </head>
 <body>
 
+<!-- Dashboard -->
 
+<div class="container">
+  <div class="row justify-content-center">
+    <img src="themes/prizelessdigital/images/logo.jpg">
+  </div>
+</div>
+
+<!-- Javascript files -->
+<script src="bootstrap-4.3.1-dist/js/bootstrap.min.js"></script>
 </body>
 </html>
