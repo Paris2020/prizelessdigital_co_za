a3f5f726b7770e4710d4040626f4a6064ce8794c
Paris2020 <nelly.moseki@amazeelabs.com>
2019-05-29 17:55:33 +0200

    PD: Troubleshooting gulpfile.js issues with localhost:3000

--- gulpfile.js
+++ gulpfile.js
@@ -6,80 +6,87 @@ var themes_folder = 'themes';
 
 var path = {
     theme:  themes_folder + '/prizelessdigital/',
-    sass:   'sass',
+    scss:   'sass',
     css:    'css',
     js:     'js'
 };
 
 //Require plugins to be in use below through functions
 var gulp          = require('gulp'),
-    util          = require('gulp-util'),
     sass          = require('gulp-sass'),
-    globbing      = require('gulp-css-globbing'),
+    sourcemaps    = require('gulp-sourcemaps'),
+    autoprefixer  = require('gulp-autoprefixer'),
     browserSync   = require('browser-sync'),
+    fs            = require('fs'),
     reload        = browserSync.reload;
 
 
 // Set paths e.g path.sass is actually themes/prizelessdigital/sass
-path.sass   = path.theme + path.sass;
+path.scss   = path.theme + path.scss;
 path.css    = path.theme + path.css;
 path.js     = path.theme + path.js;
 
-var dev = util.env.env != 'prod' ? true : false;
-var no_error_exit = false;
+
+// These are the files we want to watch
+var phpWatchFiles = '**/*.php',
+    styleWatchFiles = path.scss +'**/*.scss';
+
+
+if( fs.existsSync('./domain.json') ) {
+    var domain        = require('./domain.json');
+}
 
 /*
- - Lets define all the necessary tasks:
- -- 1) Gulp define sass task with globbing & browserSync
- -- 2) Gulp define sass-compile task
- -- 3) Gulp define compile task which calls the sass-compile task
- -- 4) Gulp define browserSync init task
- -- 6) Gulp define watch task
- -- 7) Default task which calls the watch task
- -- 8) Gulp compile task whcih calls the sass task
+- 1) This function will process the styles.scss file
+-
+- 2)
+-
+- 3)
+-
+- 4)
 */
 
+
 //1
-gulp.task('sass', function () {
+function css(){
 
-  gulp.src(path.sass + '/**/*.scss')
-    .pipe(globbing({ extensions: ['.scss'] }))
-    .pipe(no_error_exit ? sass.sync().on('error', sass.logError) : sass.sync())
-    .pipe(gulp.dest('./' + path.css))
-    .pipe(browserSync.reload({stream: true}))
-    .pipe(dev ? filter([path.css + '/**/*.css', '!' + path.css + '/animate.css']) : util.noop())
-    .pipe(dev ? duration('moved all files to /css folder') : util.noop());
-});
+    return gulp.src([path.scss + 'stytles.scss'])
+    .pipe(sourcemaps.init({loadMaps:true}))
+    .pipe(sass({
+        outputStyle: 'expanded'
+    }).on('error', path.sass.logError))
+    .pipe(autoprefixer('last 10 versions', 'ie 10'))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(path.css));
 
-
-//
-gulp.task('browser-sync', function() {
-
-  return browserSync.init(null, {
-    proxy: domain,
-    startPath: "",
-    files: path.css + '/*.css',
-    notify: false,
-    logConnections: true,
-    reloadOnRestart: true
-  });
-});
+}
 
 
-//
-gulp.task('watch', ['browser-sync'], function () {
-    no_error_exit = true;
-    gulp.watch(path.sass + '/**/*.scss', ['sass']);
-    gulp.watch(path.js + '/**/*.js').on('change', reload);
-    gulp.watch("*.html").on('change', reload);
-});
+//2
+function watch(){
+
+    browserSync.init({
+        open: 'external',
+        proxy: domain,
+        startPath: "",
+        files: path.css + '/*.css',
+        notify: false,
+        logConnections: true,
+        reloadOnRestart: true,
+    });
+    gulp.watch(styleWatchFiles, css);
+    gulp.watch(phpWatchFiles).on('change', browserSync.reload);
+}
 
 
-//
-gulp.task('default', ['watch']);
+//3
+exports.css = css;
+exports.watch = watch;
 
-//
-gulp.task('compile', ['sass']);
+
+//4
+var build = gulp.parallel(watch);
+gulp.task('default', build);
 
 
 
