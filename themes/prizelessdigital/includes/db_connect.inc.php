a6ac67295c8d50206513ffdc656c20ff750b5658
Paris2020 <nelly.moseki@amazeelabs.com>
2019-06-04 15:29:00 +0200

    PD: Move the images folder inside the themes folder

---
+++ themes/prizelessdigital/includes/db_connect.inc.php
@@ -0,0 +1,29 @@
+<?php
+
+
+  #Assign server information to connect to the database
+  $serverName = "localhost";
+  $userName = "root";
+  $password = "root";
+  $dbName = "prizeless_db";
+
+
+  #Check to see if connection to the database was made otherwise stop attempt
+  # and display errro message
+  # Else run a db select function
+  $connection = mysqli_connect($serverName, $userName, $password, $dbName);
+  if(!$connection){
+    die('Could not connect to database: ' . mysqli_error());
+  }else{
+    //echo "<script>alert('Database connection was successful!!')</script>";
+    $db_select = mysqli_select_db($connection, $dbName);
+    if(!$db_select){
+      die('Can\'t use ' . $dbName . ': ' . mysqli_error());
+    }
+  }
+
+
+
+
+
+
