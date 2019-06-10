051c43441a2baf6ba226f720b536cbdeb22163b4
Paris2020 <nelly.moseki@amazeelabs.com>
2019-06-10 19:22:12 +0200

    PD: Modified dashboard.php html5

--- themes/prizelessdigital/dashboard.php
+++ themes/prizelessdigital/dashboard.php
@@ -18,60 +18,70 @@
   <link rel="stylesheet" href="themes/prizelessdigital/styles.css">
 </head>
 
-
 <body class="grid">
-<!-- Dashboard -->
 
-
-
-<div class="container-fluid">
-  <div class="row text-center">
-    <div class="col-12">
-      <h1><?php echo $username; ?></h1>
-      <h1>Welcome to Prizeless Digital</h1>
-      <img src="themes/prizelessdigital/src/images/logo.jpg">
-    </div>
-  </div>
-</div>
-
-
-<div class="container-fluid">
-  <div class="row text-center">
-    <div class="col-sm-4">
-        <div class="card">
-          <img src="themes/prizelessdigital/src/images/construction-project-management1.jpg">
-          <div class="card-body">
-            <h5 class="card-title">Service 1</h5>
-            <p class="card-text text-left">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin lacinia ut nisi ac sagittis.</p>
-            <a href="#" class="btn btn-primary btn-lg">Read more</a>
-          </div>
+    <header class="header">
+      <div class="inner">
+        <div class="img-wrapper">
+          <img src="themes/prizelessdigital/src/images/logo.jpg"/>
         </div>
-    </div>
-    <div class="col-sm-4">
-        <div class="card">
-          <img src="themes/prizelessdigital/src/images/04-as_medical_equipment_683-ab.jpg">
-          <div class="card-body">
-            <h5 class="card-title">Service 2</h5>
-            <p class="card-text text-left">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin lacinia ut nisi ac sagittis.</p>
-            <a href="#" class="btn btn-primary btn-lg">Read more</a>
-          </div>
-        </div>
-    </div>
-    <div class="col-sm-4">
-        <div class="card">
-          <img src="themes/prizelessdigital/src/images/small_business.jpg">
-          <div class="card-body">
-            <h5 class="card-title">Service 3</h5>
-            <p class="card-text text-left">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin lacinia ut nisi ac sagittis.</p>
-            <a href="#" class="btn btn-primary btn-lg">Read more</a>
-          </div>
-        </div>
-    </div>
+      </div>
+    </header>
 
-  </div>
-</div>
+    <main class="main container-fluid">
+      <!-- Dashboard -->
+      <div class="row text-center">
+        <div class="col-12">
+          <h1><?php echo $username; ?></h1>
+          <h1>Welcome to Prizeless Digital</h1>
+          <img src="themes/prizelessdigital/src/images/logo.jpg">
+        </div>
+      </div>
 
-<!-- Javascript files -->
-<script src="themes/prizelessdigital/src/js/bootstrap-4.3.1/js/bootstrap.min.js"></script>
+      <div class="container-fluid">
+        <div class="row text-center">
+
+          <div class="col-sm-4">
+              <div class="card">
+                <img src="themes/prizelessdigital/src/images/construction-project-management1.jpg">
+                <div class="card-body">
+                  <h5 class="card-title">Service 1</h5>
+                  <p class="card-text text-left">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin lacinia ut nisi ac sagittis.</p>
+                  <a href="#" class="btn btn-primary btn-lg">Read more</a>
+                </div>
+              </div>
+          </div>
+          <div class="col-sm-4">
+              <div class="card">
+                <img src="themes/prizelessdigital/src/images/04-as_medical_equipment_683-ab.jpg">
+                <div class="card-body">
+                  <h5 class="card-title">Service 2</h5>
+                  <p class="card-text text-left">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin lacinia ut nisi ac sagittis.</p>
+                  <a href="#" class="btn btn-primary btn-lg">Read more</a>
+                </div>
+              </div>
+          </div>
+          <div class="col-sm-4">
+              <div class="card">
+                <img src="themes/prizelessdigital/src/images/small_business.jpg">
+                <div class="card-body">
+                  <h5 class="card-title">Service 3</h5>
+                  <p class="card-text text-left">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin lacinia ut nisi ac sagittis.</p>
+                  <a href="#" class="btn btn-primary btn-lg">Read more</a>
+                </div>
+              </div>
+          </div>
+
+        </div>
+      </div>
+
+    </main>
+    <footer class="footer">
+      <div class="inner">
+
+      </div>
+    </footer>
+    <!-- Javascript files -->
+    <script src="themes/prizelessdigital/src/js/bootstrap-4.3.1/js/bootstrap.min.js"></script>
 </body>
 </html>
