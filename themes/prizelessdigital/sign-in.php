eab8e4db60fb49d40accbfafabe7d2195e785e61
Paris2020 <nelly.moseki@amazeelabs.com>
2019-06-10 19:25:41 +0200

    PD: Troubleshooting html in sign-in.php

--- themes/prizelessdigital/sign-in.php
+++ themes/prizelessdigital/sign-in.php
@@ -17,24 +17,46 @@
 </head>
 <body class="grid">
 
-<!-- Signin Form -->
 
-<div class="container">
-  <div class="row justify-content-center">
-    <img src="themes/prizelessdigital/src/images/logo.jpg"/>
-    <form action="./../../includes/signin.inc.php" method="POST">
+  <header class="header">
+    <div class="inner">
+      <div class="img-wrapper">
+        <img src="themes/prizelessdigital/src/images/logo.jpg"/>
+      </div>
+    </div>
+  </header>
 
-      <input type="email" name="email" class="form-control" placeholder="Enter email address"><br>
-      <input type="password" name="password" class="form-control" placeholder="Enter password"><br>
-      <input type="submit" name="submit" value="Sign In" class="btn btn-primary"><br><br>
-      <span>OR</span><br><br>
-      <a href="index.php" class="link">Signup if you don't have an account</a>
-    </form>
-  </div>
-</div>
+  <!-- Signin Form -->
+  <main class="main container">
+    <div class="content-center">
 
+      <div class="form-wrapper">
+        <div class="form-header">
+          <h3>Signup</h3>
+          <p>Be a part of our company database...</p>
+        </div>
+        <div class="form-body">
+          <form action="./../../includes/signin.inc.php" method="POST">
 
-<!-- Javascript files -->
-<script src="themes/prizelessdigital/src/js/bootstrap-4.3.1/js/bootstrap.min.js"></script>
+            <input type="email" name="email" class="form-control" placeholder="Enter email address"><br>
+            <input type="password" name="password" class="form-control" placeholder="Enter password"><br>
+            <input type="submit" name="submit" value="Sign In" class="btn btn-primary"><br><br>
+            <span>OR</span><br><br>
+            <a href="index.php" class="link">Signup if you don't have an account</a>
+          </form>
+        </div>
+      </div>
+
+    </div>
+  </main>
+
+  <footer class="footer">
+    <div class="inner">
+
+    </div>
+  </footer>
+
+  <!-- Javascript files -->
+  <script src="themes/prizelessdigital/src/js/bootstrap-4.3.1/js/bootstrap.min.js"></script>
 </body>
 </html>
