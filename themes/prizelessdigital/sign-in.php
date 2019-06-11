de32080e57e9d24db2200668c20c2d689d006b00
Paris2020 <nelly.moseki@amazeelabs.com>
2019-06-11 20:10:56 +0200

    PD: Modified sign-in.php file

--- themes/prizelessdigital/sign-in.php
+++ themes/prizelessdigital/sign-in.php
@@ -1,7 +1,8 @@
 <?php
 
   session_start();
-  include './../../includes/db_connect.inc.php';
+  dirname(__FILE__ . 'includes/db_connect.inc.php');
+
 ?>
 
 <!DOCTYPE html>
@@ -12,6 +13,7 @@
   <meta name="description" content="Sign up and login form.">
   <meta name="author" content="Nelly Moseki, Junior Frontend Developer">
   <title>Prizeless Digital Assessment</title>
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
   <link rel="stylesheet" href="themes/prizelessdigital/src/css/bootstrap-4.3.1/css/bootstrap.min.css">
   <link rel="stylesheet" href="themes/pricelessdigital/styles.css">
 </head>
@@ -32,17 +34,21 @@
 
       <div class="form-wrapper">
         <div class="form-header">
-          <h3>Signup</h3>
-          <p>Be a part of our company database...</p>
+          <h3>Signin</h3>
+          <p>See whats going at Prizeless Digital</p>
         </div>
         <div class="form-body">
           <form action="./../../includes/signin.inc.php" method="POST">
 
-            <input type="email" name="email" class="form-control" placeholder="Enter email address"><br>
-            <input type="password" name="password" class="form-control" placeholder="Enter password"><br>
-            <input type="submit" name="submit" value="Sign In" class="btn btn-primary"><br><br>
-            <span>OR</span><br><br>
-            <a href="index.php" class="link">Signup if you don't have an account</a>
+            <input type="email" name="email" class="form-control">
+            <input type="password" name="password" class="form-control">
+
+            <div class="btn-wrapper">
+              <input type="submit" name="submit" value="Sign In" class="btn btn-primary">
+              <span>OR</span>
+              <a href="index.php" class="link">Signup if you don't have an account</a>
+            </div>
+
           </form>
         </div>
       </div>
