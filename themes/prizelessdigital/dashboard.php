039d988c4c744886d1b894318745cb53a5b77a7d
Paris2020 <nelly.moseki@amazeelabs.com>
2019-06-18 17:40:43 +0200

    PD: Troubleshooting custom btn styling

--- themes/prizelessdigital/dashboard.php
+++ themes/prizelessdigital/dashboard.php
@@ -14,9 +14,8 @@
   <meta name="description" content="Sign up and login form.">
   <meta name="author" content="Nelly Moseki, Junior Frontend Developer">
   <title>Prizeless Digital</title>
-  <link href="src/css/fontawesome-5.9.0/css/fontawesome.css" rel="stylesheet">
-  <link rel="stylesheet" href="themes/prizelessdigital/src/css/bootstrap-4.3.1/css/bootstrap.min.css">
-  <link rel="stylesheet" href="themes/prizelessdigital/styles.css">
+  <link rel="stylesheet" href="src/css/bootstrap-4.3.1/css/bootstrap.min.css">
+  <link rel="stylesheet" href="styles.css">
 </head>
 
 <body class="grid">
@@ -24,7 +23,7 @@
     <header class="header">
       <div class="inner">
         <div class="img-wrapper">
-          <img src="themes/prizelessdigital/src/images/logo.jpg"/>
+          <img src="src/images/logo.jpg"/>
         </div>
       </div>
     </header>
@@ -33,9 +32,8 @@
       <!-- Dashboard -->
       <div class="row text-center">
         <div class="col-12">
-          <h1><?php echo $username; ?></h1>
           <h1>Welcome to Prizeless Digital</h1>
-          <img src="themes/prizelessdigital/src/images/logo.jpg">
+          <h1><?php echo $username; ?></h1>
         </div>
       </div>
 
@@ -44,31 +42,37 @@
 
           <div class="col-sm-4">
               <div class="card">
-                <img src="themes/prizelessdigital/src/images/construction-project-management1.jpg">
+                <div class="img-wrapper">
+                  <img src="src/images/construction-project-management1.jpg"/>
+                </div>
                 <div class="card-body">
                   <h5 class="card-title">Service 1</h5>
-                  <p class="card-text text-left">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin lacinia ut nisi ac sagittis.</p>
-                  <a href="#" class="btn btn-primary btn-lg">Read more</a>
+                  <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin lacinia ut nisi ac sagittis.</p>
+                  <a href="#" class="btn btn-large">Read more</a>
                 </div>
               </div>
           </div>
           <div class="col-sm-4">
               <div class="card">
-                <img src="themes/prizelessdigital/src/images/04-as_medical_equipment_683-ab.jpg">
+                <div class="img-wrapper">
+                  <img src="src/images/04-as_medical_equipment_683-ab.jpg"/>
+                </div>
                 <div class="card-body">
                   <h5 class="card-title">Service 2</h5>
-                  <p class="card-text text-left">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin lacinia ut nisi ac sagittis.</p>
-                  <a href="#" class="btn btn-primary btn-lg">Read more</a>
+                  <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin lacinia ut nisi ac sagittis.</p>
+                  <a href="#" class="btn btn-large">Read more</a>
                 </div>
               </div>
           </div>
           <div class="col-sm-4">
               <div class="card">
-                <img src="themes/prizelessdigital/src/images/small_business.jpg">
+                <div class="img-wrapper">
+                  <img src="src/images/small_business.jpg"/>
+                </div>
                 <div class="card-body">
                   <h5 class="card-title">Service 3</h5>
-                  <p class="card-text text-left">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin lacinia ut nisi ac sagittis.</p>
-                  <a href="#" class="btn btn-primary btn-lg">Read more</a>
+                  <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin lacinia ut nisi ac sagittis.</p>
+                  <a href="#" class="btn btn-large">Read more</a>
                 </div>
               </div>
           </div>
@@ -83,6 +87,6 @@
       </div>
     </footer>
     <!-- Javascript files -->
-    <script src="themes/prizelessdigital/src/js/bootstrap-4.3.1/js/bootstrap.min.js"></script>
+    <script src="src/js/bootstrap-4.3.1/js/bootstrap.min.js"></script>
 </body>
 </html>
